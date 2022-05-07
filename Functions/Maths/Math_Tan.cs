5288a5cf2ce4816ed38f82b709817270165fca6f
Mervinpaismakeswindows14 <mervinpaismakeswindows14@gmail.com>
2022-05-07 23:12:21 +0400

    fixed maths sin and tan (because i was lazy and now i fixed 'em)

--- Functions/Maths/Math_Tan.cs
+++ Functions/Maths/Math_Tan.cs
@@ -19,14 +19,14 @@ namespace Easy14_Coding_Language
             code_part = code_part_unedited.TrimStart();
 
             string expression = code_part.ToLower();
-            if (expression is null && !expression.Contains("cos")) {
-                Console.WriteLine("ERROR; Can't do Cos, please check your code and fix the error");
+            if (expression is null && !expression.Contains("tan")) {
+                Console.WriteLine("ERROR; Can't do Tan, please check your code and fix the error");
                 return 0.0;
             }
             expression = expression.Replace(" ", "");
             expression = expression.Replace("=", "");
             //Console.WriteLine(expression);
-            int theNumber = Convert.ToInt32(expression.Replace("cos(", "").Replace(")", ""));
+            int theNumber = Convert.ToInt32(expression.Replace("tan(", "").Replace(")", ""));
             //Console.WriteLine(theNumber);
             var result = Math.Tan(theNumber);
             return result;
