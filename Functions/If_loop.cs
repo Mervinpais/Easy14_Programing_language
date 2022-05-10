b41ca6bff6aa319d4e24584b0f74b75b2573d5c9
Mervinpaismakeswindows14 <mervinpaismakeswindows14@gmail.com>
2022-05-10 23:00:24 +0400

    Fixed stuff

--- Functions/If_loop.cs
+++ Functions/If_loop.cs
@@ -15,7 +15,7 @@ namespace Easy14_Coding_Language
         //Above needed as functions like 'compileCode' in Program.cs cant be accessed here and instead of copying it to other functions, just make an object of it 
         //and use it's 'compileCode_forOtherFiles' function to get 'compileCode' (because 'compileCode' is static, we need another function that is not static to access
         //the static function, kinda smart in my opinion), its kind of a bad way of doing it but it the easy way and has no error with it :|
-        public void interperate(string code_part, string[] lines, string[] textArray, string fileloc)
+        public void interperate(string code_part, string[] lines, string[] textArray, string fileloc, int lineIDX = 0, bool isInAMethod = false, string methodName = "}")
         {
             string code_part_unedited;
             //string textToPrint;
@@ -24,8 +24,23 @@ namespace Easy14_Coding_Language
             string[] if_lines = lines;
             int end_line_IDX = 0;
             int line_counterr = 1;
-            List<string> if_lines_list = new List<string>(if_lines);
 
+            //Take all lines and convert them to a list
+            List<string> if_lines_list = new List<string>(if_lines);
+            List<string> usings_lines_list = new List<string>(if_lines_list);
+            //below is a way to get the usings and then use them for other stuff
+            foreach (string line__ in if_lines)
+            {
+                if (!line__.StartsWith("using"))
+                {
+                    usings_lines_list.RemoveRange(line_counterr, usings_lines_list.Count - line_counterr);
+                    break;
+                }
+                line_counterr++;
+            }
+
+            line_counterr = 1;
+            //Below is a method to find the code under the if statement
             foreach (string line__ in if_lines)
             {
                 if (line__ == code_part)
@@ -62,19 +77,114 @@ namespace Easy14_Coding_Language
             string if_Line = if_lines_list[0];
             if_Line = if_Line.Substring(2);
             if_Line = if_Line.Substring(1, if_Line.Length - 2);
-            string obj1 = null;
-            bool obj1_variable = false;
-            if (if_Line.Contains("=="))
+            
+            string obj1 = null; bool obj1_variable = false;
+            string obj2 = null; bool obj2_variable = false;
+
+            if (if_Line.Contains("==")) {
                 obj1 = if_Line.Substring(0, if_Line.IndexOf("==") - 0);
-            if (if_Line.Contains("!="))
-                obj1 = if_Line.Substring(0, if_Line.IndexOf("!=") - 0);
-            string obj2 = null;
-            bool obj2_variable = false;
-            if (if_Line.Contains("=="))
                 obj2 = if_Line.Substring(if_Line.IndexOf("==") + 2).TrimEnd();
-            if (if_Line.Contains("!="))
+            }
+            else if (if_Line.Contains("!=")) {
+                obj1 = if_Line.Substring(0, if_Line.IndexOf("!=") - 0);
                 obj2 = if_Line.Substring(if_Line.IndexOf("!=") + 2).TrimEnd();
+            }
+
+            obj1 = obj1.TrimStart().TrimEnd();
+            obj2 = obj2.TrimStart().TrimEnd();
+            
             if (if_Line.Contains("=="))
+            {
+                string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @$"\EASY14_Variables_TEMP";
+                foreach (string ffile in Directory.GetFiles(dir))
+                {
+                    if (ffile.Substring(ffile.LastIndexOf(@"\")).Replace(@"\", "").Replace(".txt", "") == obj1)
+                    { obj1_variable = true; }
+                }
+                foreach (string ffile in Directory.GetFiles(dir))
+                {
+                    if (ffile.Substring(ffile.LastIndexOf(@"\")).Replace(@"\", "").Replace(".txt", "") == obj2)
+                    { obj2_variable = true; }
+                }
+
+                List<string> someLINEs = null;
+                if (textArray == null && fileloc != null) someLINEs = new List<string>(File.ReadAllLines(fileloc));
+                else if (textArray != null && fileloc == null) someLINEs = new List<string>(textArray);
+                int lin_count = 1;
+                foreach (string x in someLINEs)
+                {
+                    lin_count++;
+                    if (!x.StartsWith("using") && x != "" && x != null)
+                    {
+                        break;
+                    }
+                }
+
+                lin_count = lin_count - 2;
+
+                string obj1_fileContent = null;
+                string obj2_fileContent = null;
+                if (obj1_variable) {
+                    obj1_fileContent = File.ReadAllText(dir + @$"\{obj1}.txt");
+                }
+                if (obj2_variable) {
+                    obj2_fileContent = File.ReadAllText(dir + @$"\{obj2}.txt");
+                }
+
+                if (obj1_variable == true && obj2_variable == false)
+                {
+                    if (obj1_fileContent == obj2)
+                    {
+                        List<string> e_code = if_lines_list.GetRange(1, if_lines_list.Count - 1);
+                        List<string> usings_code = someLINEs.GetRange(0, lin_count);
+                        usings_code.AddRange(e_code);
+                        e_code = usings_code;
+                        prog.compileCode_fromOtherFiles(null, e_code.ToArray());
+                    }
+                }
+                else if (obj1_variable == false && obj2_variable == true)
+                {
+                    if (obj1 == obj2_fileContent)
+                    {
+                        List<string> e_code = if_lines_list.GetRange(1, end_line_IDX - 2);
+                        List<string> usings_code = someLINEs.GetRange(0, lin_count);
+                        usings_code.AddRange(e_code);
+                        e_code = usings_code;
+                        prog.compileCode_fromOtherFiles(null, e_code.ToArray());
+                    }
+                }
+                else if (obj1_variable == true && obj2_variable == true)
+                {
+                    if (obj1_fileContent == obj2_fileContent)
+                    {
+                        List<string> e_code = if_lines_list.GetRange(1, end_line_IDX - 2);
+                        List<string> usings_code = someLINEs.GetRange(0, lin_count);
+                        usings_code.AddRange(e_code);
+                        e_code = usings_code;
+                        prog.compileCode_fromOtherFiles(null, e_code.ToArray());
+                    }
+                }
+                else if (obj1_variable == false && obj2_variable == false)
+                {
+                    if (obj1 == obj2)
+                    {
+                        List<string> e_code = if_lines_list.GetRange(1, end_line_IDX - 2);
+                        List<string> usings_code = someLINEs.GetRange(0, lin_count);
+                        usings_code.AddRange(e_code);
+                        e_code = usings_code;
+                        prog.compileCode_fromOtherFiles(fileloc, e_code.ToArray(), lineIDX);
+                    }
+                }
+                understuff.RemoveRange(0, end_line_IDX);
+
+                List<string> code_toContinueExceuting = new List<string>();
+                code_toContinueExceuting.AddRange(usings_lines_list);
+                code_toContinueExceuting.AddRange(understuff);
+
+                //int lineIDX_underpart = lineIDX + (code_toContinueExceuting.ToArray().Length - lineIDX);
+                prog.compileCode_fromOtherFiles(null, code_toContinueExceuting.ToArray());
+            }
+            else if (if_Line.Contains("!="))
             {
                 string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @$"\EASY14_Variables_TEMP";
                 foreach (string ffile in Directory.GetFiles(dir))
@@ -107,9 +217,10 @@ namespace Easy14_Coding_Language
 
                 lin_count = lin_count - 2;
 
+                string obj1_fileContent = File.ReadAllText(dir + @$"\{obj1}.txt");
                 if (obj1_variable == true && obj2_variable == false)
                 {
-                    if (File.ReadAllText(dir + @$"\{obj1}.txt") == obj2)
+                    if (obj1_fileContent != obj2)
                     {
                         List<string> e_code = if_lines_list.GetRange(1, end_line_IDX - 2);
                         List<string> usings_code = someLINEs.GetRange(0, lin_count);
@@ -120,7 +231,7 @@ namespace Easy14_Coding_Language
                 }
                 else if (obj1_variable == false && obj2_variable == true)
                 {
-                    if (obj1 == File.ReadAllText(dir + @$"\{obj2}.txt"))
+                    if (obj1 != File.ReadAllText(dir + @$"\{obj2}.txt"))
                     {
                         List<string> e_code = if_lines_list.GetRange(1, end_line_IDX - 2);
                         List<string> usings_code = someLINEs.GetRange(0, lin_count);
@@ -131,7 +242,7 @@ namespace Easy14_Coding_Language
                 }
                 else if (obj1_variable == true && obj2_variable == true)
                 {
-                    if (File.ReadAllText(dir + @$"\{obj1}.txt") == File.ReadAllText(dir + @$"\{obj2}.txt"))
+                    if (obj1_fileContent != File.ReadAllText(dir + @$"\{obj2}.txt"))
                     {
                         List<string> e_code = if_lines_list.GetRange(1, end_line_IDX - 2);
                         List<string> usings_code = someLINEs.GetRange(0, lin_count);
@@ -142,17 +253,46 @@ namespace Easy14_Coding_Language
                 }
                 else if (obj1_variable == false && obj2_variable == false)
                 {
-                    if (obj1 == obj2)
+                    if (obj1 != obj2)
                     {
                         List<string> e_code = if_lines_list.GetRange(1, end_line_IDX - 2);
                         List<string> usings_code = someLINEs.GetRange(0, lin_count);
                         usings_code.AddRange(e_code);
                         e_code = usings_code;
-                        prog.compileCode_fromOtherFiles(null, e_code.ToArray());
+                        prog.compileCode_fromOtherFiles(fileloc, e_code.ToArray(), lineIDX);
                     }
                 }
+
+                int part_to_continue_at = 1;
+                foreach (string e in understuff)
+                {
+                    if (e == methodName + "();")
+                    {
+                        break;
+                    }
+                    part_to_continue_at++;
+                }
                 understuff.RemoveRange(0, end_line_IDX - 1);
-                prog.compileCode_fromOtherFiles(null, understuff.ToArray());
+                List<string> code_toContinueExceuting = new List<string>();
+                code_toContinueExceuting.AddRange(usings_lines_list);
+                code_toContinueExceuting.AddRange(understuff);
+
+                part_to_continue_at = 1;
+                /*
+                foreach (string e in code_toContinueExceuting)
+                {
+                    if (e == methodName + "();")
+                    {
+                        code_toContinueExceuting.RemoveAt(part_to_continue_at- 1);
+                        break;
+                    }
+                    part_to_continue_at++;
+                }*/
+
+                //int lineIDX_underpart = lineIDX + (code_toContinueExceuting.ToArray().Length - lineIDX);
+                if (isInAMethod == true) {
+                    prog.compileCode_fromOtherFiles(null, code_toContinueExceuting.ToArray());
+                }
             }
         }
     }
--- README.md
+++ README.md
@@ -7,41 +7,77 @@ Basically a mashup of Python + C#
 	</head>
 </html>
 
+<hr>
+
 # Main Stuff
 Here is what you need to know
 
-* This is just like any other hight level language
+* This is just like any other high level language
 * Runs on C#
-* Isn't a complete build but will be patching some bugs in 1.1
-* Currently all programming statements/keywords
-	* If
-	* while
-	* var
-	* wait
-	* Console.-
-		* Print
-		* Input
-	* FileSystem.-
-		* MakeFile
-		* ReadFile
-		* RenameFile
-		* WriteFile
-		* DeleteFile
-		* DeleteFolder
-	* Network.-
-		* Ping
-	* // (Comment)
-* Basic Operators (Math)
-	* + (Add)
-	* - (Subtract)
-	* / (Divide)
-	* * (Multiply)
-	* % (Module)
-# Version 1.1
+<hr>
 
-The Cool Stuff and Bugfix's arrived,
+# Version 1.2
 
-* Fixed bugs with Print() and Input()
-* Updated The Repair Tool(s)
-* Optimised 5-10% of the code
-* Added COS, SIN, TAN to the maths operator list
+The Cool Stuff and Bugfixs arrived,
+
+* Fixed bugs with functions (many 'functions' aka statements were fixed)
+* Optimised a bit of the code
+* METHODS! (Parameters are not yet there, please wait till version 1.3 or 1.4) Example;
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;func methodName() { <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("This was printed from a method!"); <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;methodName();
+
+<hr>
+
+# Known Issues
+
+- When using a while/if statement inside another if/while statement, the whole code bugs out and just does whatever example;
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if condition == "true" { <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if condition2 == "true" <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{ <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("yes"); <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+
+### This is Causes major bugs
+
+- A Fix will be coming soon, probably in version 1.3 or 1.4, if not, the bug may be too much to handle
+
+## Another similer bug
+
+- When using an if/while statement inside a method, the if loop has to get whatever is below to continue, and since im so bad at programming (aka "im lazy so i make code without documenting it so i cant understand it later", known as the ILSIMCWDISICUIL condtion) example;
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;func methodName() { <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if condition2 == "true" <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{ <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("yes"); <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+
+# Why are there bugs?!
+There are many reasons, each for different situations;
+
+A) For Companies;
+  1. Lazy Workers
+  2. Not paided well workers
+  3. Not Clear Instruction
+  4. Bad bosses
+  5. Sh!t code from 1990 (or any ancient code from 10 years ago)
+
+B) For Independant People
+  1. Lazy
+  2. Bad Code
+  3. Bad Metal State
+  4. Not Caring for making the code easy to undestand
+  5. Sh!t code  from 1990 (or any ancient code from 10 years ago)
+
+Im more of a B3 and a bit of B4, but you choose yourself, you may need to improve by making the time to learn and code about the language you want to code in.
+
+<hr>
+
+# _Quote_ of the Version
+_"But Can you this"_ - "But can you do this" song by Lolnein
