6b4c0b9f527bb70bf49f51b102d335b4c1b15c9f
Mervin pais <mervinpaismakeswindows14@gmail.com>
2022-06-22 10:51:17 +0400

    Revert "added parameters support for custom functions"
    This reverts commit 1a56dd4045f0bbfe48875faa142615f40f3c85c8.

--- Functions/MyCustomFunction/myFunc.cs
+++ Functions/MyCustomFunction/myFunc.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 
-namespace Easy14_Programming_Language //this should only be the Project Namespace, dont change it!
+namespace Easy14_Programming_Language
 {
-    public class myFunc //change this to whatever name you like
+    public class myFunc
     {
-        public void interperate(string para1 = "null", string para2 = "null") //keep the same function name!! dont change!!
+        public void interperate()
         {
-            Console.WriteLine("Hello! this is the test function!, you may edit/copy + paste it for whatever you want");
-            Console.WriteLine("and yes, this does mean you can.. MAKE YOUR OWN FUNCTIONS!");
-            Console.WriteLine("If you need help, refer to the documentation");
-
-            Console.WriteLine("\n First (string) Param; " + para1);
-            Console.WriteLine("\n Second (string) Param; " + para2);
+            Console.WriteLine("Ears are cute!");
         }
 
     }
