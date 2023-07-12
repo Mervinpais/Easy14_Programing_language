fba1c5e1219ce1d64867c03ba5b8c6c9c9c43f3c
Mervinpais14 <mervinpaismakeswindows14@gmail.com>
2023-07-12 20:57:48 +0400

    last dump before revamp i will remake this language soon

---
+++ Easy14_Programming_language/Functions/BaseFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Easy14_Programming_Language
+{
+    public static class BaseFunctions
+    {
+        public static class Sys
+        {
+            public static void Write(string text, bool newLine)
+            {
+                if (newLine)
+                {
+                    Console.WriteLine(text);
+                }
+                else
+                {
+                    Console.Write(text);
+                }
+            }
+        }
+    }
+}
